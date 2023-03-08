03465a0df7a12b153d26d35f2445d2e4ff34e5a4
Dany Cervantes <danycervantesq@gmail.com>
2023-03-07 21:24:55 -0500

    Update notflix.js

--- notflix.js
+++ notflix.js
@@ -1,46 +1,26 @@
 const video = document.getElementById("video");
-
-let interact = false; //browser prevents autoplay
+const showcase = document.querySelector(".showcase");
 
 
 function show() {
-
-    /*
-    video.style.position = "absolute"; 
-    video.style.left = "0";
-    video.style.top = "0";
-
-    video.style.height = "100vh";
-    video.style.objectFit = "cover";
-    video.style.width = "100vw";
-    video.style.zIndex = "-1";
-    */
    
     video.play();
+
 }
 
 
-document.body.addEventListener("click", function() {
-
-    interact = true;
+showcase.addEventListener("click", function() {
     
-    if(interact && video.paused) {
+    if(video.paused) {
 
         show();
+
+    } else {
+
+        video.pause();
     }
 
 });
-
-document.body.addEventListener("mouseover", function() {
-
-    interact = true;
-
-    if(interact && video.paused) {
-
-        show();
-    }
-});
-
 
 
 //window.onload = function() {  };
