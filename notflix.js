3060dee8124935e1e32b8c955edb521f654835bf
Dany Cervantes <danycervantesq@gmail.com>
2023-03-12 22:10:55 -0400

    Update notflix.js

--- notflix.js
+++ notflix.js
@@ -6,8 +6,35 @@ let playIcon = document.getElementById("play-icon");
 playButton.innerHTML = "Play";
 playIcon.className = "fa fa-play";
 
+let selection = [
+    {
+        name: "Knights Of Sidonia",
+        logo: 'url(assets/logos/Sidonia-logo.png',
+        img: 'url(assets/pictures/Knights-Sidonia.png)',
+        trailer1: 'assets/trailers/Knights-Sidonia-1.mp4',
+        trailer2: 'assets/trailers/Knights-Sidonia-2.mp4'
+    }
+];
+
 
 function theater() {
+
+    function flip() {
+        let coin = randomRange(1,2);
+        
+        if(coin == 1) {
+            video.src = selection[0].trailer1;
+        } else {
+            video.src = selection[0].trailer2;
+        };
+    };
+
+    flip();
+
+}
+
+
+function buttonUI() {
    
     if(video.paused) {
 
@@ -27,13 +54,23 @@ function theater() {
 }
 
 
+//Returns a random number within a chosen range
+function randomRange(min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+//Math.floor() rounds down to the nearest whole number  e.i. 10 = 0 - 9  
+//Math.random() returns a random decimal between 0 - 0.99
+}
+
+
 showcase.addEventListener("click", function() {
     
-    theater();
+    buttonUI();
 
 });
 
 
 window.onload = function() { 
+
+    theater();
 
  };
