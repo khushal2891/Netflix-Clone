b55abf3ce053cc6b654e62243c8851606bdc4a1f
Dany Cervantes <danycervantesq@gmail.com>
2023-03-16 21:51:04 -0400

    Update notflix.js

--- notflix.js
+++ notflix.js
@@ -1,54 +1,30 @@
-const video = document.getElementById("video");
-const showcase = document.querySelector(".showcase");
-let carousel1 = document.getElementById("carousel1");
-let showcaseImg = document.querySelector(".showcase-img");
-
-
-let playButton = document.getElementById("play-button");
-let playIcon = document.getElementById("play-icon");
-playButton.innerHTML = "Play";
-playIcon.className = "fa fa-play";
-
-let selection = [
+const array = [
+    {
+        name: "Avengers: Infinity War",
+        logo: `assets/logos/infinity-logo.png`,
+        img: `assets/pictures/infinity-war.png`,
+        trailer: `assets/trailers/avengers-endgame.mp4`,
+    },
     {
         name: "Knights Of Sidonia",
         logo: 'assets/logos/Sidonia-logo.png',
         img: 'assets/pictures/Knights-Sidonia.png',
         trailer: 'assets/trailers/Knights-Sidonia-1.mp4',
-        trailer2: 'assets/trailers/Knights-Sidonia-2.mp4'
-    },
-    {
-        name: "Avengers: Infinity War",
-        logo: `assets/logos/infinity-logo.png`,
-        img: `assets/pictures/infinity-war.png`,
-        trailer: `assets/trailers/avengers-endgame.mp4`
+        trailer2: 'assets/trailers/Knights-Sidonia-2.mp4',
     }
+    
 ];
 
-let num = randomRange(0, selection.length - 1); //sets random number within array size
+const catalogue = document.querySelector(".catalogue");
+const showcase = document.querySelector(".showcase");
+const video = document.getElementById("video");
 
-
-function theater() {
-    
-    video.src = selection[num].trailer;
-    showcaseImg.src= selection[num].logo;
-    
-    
-    function flip() {
-        let coin = randomRange(1,2);
-        
-        if(coin == 1) {
-            video.src = selection[num].trailer;
-        } else {
-            video.src = selection[num].trailer2;
-        };
-    };
-    
-    if(selection[num].name == "Knights Of Sidonia") {
-
-        flip();
-    }
-}
+let carousel1 = document.getElementById("carousel1");
+let num = randomRange(0, array.length - 1); //sets random number within array size
+let playButton = document.getElementById("play-button");
+let playIcon = document.getElementById("play-icon");
+let selection;
+let showcaseImg = document.querySelector(".showcase-img");
 
 
 function buttonUI() {
@@ -71,8 +47,12 @@ function buttonUI() {
 
 function display() {
 
-    selection.forEach(obj => {
-        let figure = document.createElement("figure");
+    playButton.innerHTML = "Play";
+    playIcon.className = "fa fa-play";
+
+    array.forEach(obj => {
+        let figure = document.createElement("figure"); 
+        figure.classList.add("selection");
         let img = new Image();
         img.src = obj.img;
         figure.appendChild(img);
@@ -81,6 +61,8 @@ function display() {
         figure.appendChild(figName);
         carousel1.appendChild(figure);
     });
+
+    selection = document.querySelectorAll(".selection");
 
 }
 
@@ -93,10 +75,37 @@ function randomRange(min,max) {
 }
 
 
-showcase.addEventListener("click", function() {
+function theater() {
     
-    buttonUI();
+    showcaseImg.src = array[num].logo;
+        
+    switch(array[num].name) {
+        case "Knights Of Sidonia":
+            flip();
+            break;
+        default:
+            video.src = array[num].trailer;
+            break;
+    }
 
+    function flip() {
+
+        let coin = randomRange(1,2);
+        
+        if(coin == 1) {
+
+            video.src = array[num].trailer;
+        } else {
+            video.src = array[num].trailer2;
+        };
+    };
+
+}
+
+
+showcase.addEventListener("click", function() {
+
+    buttonUI();
 });
 
 
@@ -104,5 +113,15 @@ window.onload = function() {
 
     display();
     theater();
+
+    //Took two days, after a 9-5,to figure this out
+    for(let i = 0; i < selection.length; i++) {
+
+        selection[i].addEventListener("click", function() {
+            num = i;
+            theater();
+            buttonUI();
+        });
+    };
 
  };
